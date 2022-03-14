b931767dd7014b63f53002e406cc7e24d457f52e
Alexandre-st <alexandre.saintprix@gmail.com>
2022-03-14 14:51:49 +0100

    Fix the issue on the list, an id is unique and i reused the same id at the creation of the input

--- src/index.js
+++ src/index.js
@@ -36,13 +36,11 @@ const showList = (todo) => {
   // Create the checkbox with label to hide the checkbox
   const label = document.createElement('label');
   label.classList.add('list-check');
-  label.setAttribute('for', 'taskDone');
   item.appendChild(label);
   
   // Create the input with checkbox type
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
-  checkbox.setAttribute('id', 'taskDone');
   checkbox.addEventListener('click', taskDone);
   label.appendChild(checkbox);
   
@@ -56,6 +54,7 @@ const showList = (todo) => {
   text.innerText = todo.text;
   item.appendChild(text);
   
+  // Push to the allTask array.
   list.appendChild(item);
   allTask.push(item);
 };
